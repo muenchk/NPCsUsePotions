33eb8c564436a4623051b99de716a0a3acdac47e
Kai Münch <kai.muench@outlook.de>
2025-09-01 12:56:28 +0200

    update

--- include/Logging.h
+++ include/Logging.h
@@ -96,7 +96,7 @@
 		((void)0);
 #else
 #	define LogConsole(c_str) \
-		RE::ConsoleLog::GetSingleton()->Print(c_str); // ((void)0);
+		((void)0); // RE::ConsoleLog::GetSingleton()->Print(c_str);
 #endif
 
 
